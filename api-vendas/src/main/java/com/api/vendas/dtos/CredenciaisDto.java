5deae13fb2b55e8f266448ef665c970ebd02843e
ViniciusNeto <92762759+ViniciusNeto@users.noreply.github.com>
2024-08-18 23:57:34 -0300

    Add files via upload

---
+++ api-vendas/src/main/java/com/api/vendas/dtos/CredenciaisDto.java
@@ -0,0 +1,12 @@
+package com.api.vendas.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CredenciaisDto {
+
+	private String login;
+	private String senha;
+}
---
+++ api-vendas/src/main/java/com/api/vendas/dtos/TokenDto.java
@@ -0,0 +1,16 @@
+package com.api.vendas.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDto {
+
+	private String token;
+	private UsuarioDto usuario;
+}
